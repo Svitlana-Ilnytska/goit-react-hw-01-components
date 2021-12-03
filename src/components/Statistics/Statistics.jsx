8fe4058fed42baebb208c213bde07b2e990899b1
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2021-12-04 01:13:57 +0200

    added Profile and Statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import css from "./Statistics.module.css";
+
+const Statistics = ({title, stats }) => (
+  <section className={css.statistics}>
+    <h2 className={css.title}>{title}</h2>
+
+    <ul>
+      {stats.map((stat) => (
+        <li key={stat.id}>
+          <span className={css.label}>{stat.name}</span>
+          <span className={css.percentage}>{stat.percentage}</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Statistics.defaultProps = {
+  title: "",
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+    
+      name: PropTypes.string.isRequired,
+      percentage: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Statistics;
