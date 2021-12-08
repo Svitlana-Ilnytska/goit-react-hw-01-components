e545b6d321282f876e2f37c0ebfbd99a54766556
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2021-12-08 20:39:44 +0200

    added jsonComponents in databaseFolder

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Statistics from "./components/Statistics/Statistics";
 import FriendList from "./components/FriendList/FriendList";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
-import user from "./user.json";
-import data from "./data.json";
-import friends from "./friends.json";
-import transactions from "./transactions.json";
+import user from "./database/user.json";
+import data from "./database/data.json";
+import friends from "./database/friends.json";
+import transactions from "./database/transactions.json";
 
 export default function App() {
   return (
